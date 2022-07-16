ca31493a72a0a9937d04362bb37ff4f5deead819
Volodymyr-Boiechko <boiechko17@gmail.com>
2022-07-16 22:05:58 +0300

    added basic configs

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserNoAuthGuard} from "./guards/user-no-auth.guard";
+import {LoginComponent} from "./pages/auth/login/login.component";
+import {UserAuthGuard} from "./guards/user-auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+    canLoad: [UserNoAuthGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canLoad: [UserNoAuthGuard]
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/portal/home/home.module').then(m => m.HomeModule),
+    canLoad: [UserAuthGuard]
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}

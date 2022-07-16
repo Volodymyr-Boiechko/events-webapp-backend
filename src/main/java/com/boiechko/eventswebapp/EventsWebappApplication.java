ee24699000735ac58808e1f9e90d0292af857639
Volodymyr-Boiechko <boiechko17@gmail.com>
2022-07-16 18:02:51 +0300

    init commit

---
+++ src/main/java/com/boiechko/eventswebapp/EventsWebappApplication.java
@@ -0,0 +1,13 @@
+package com.boiechko.eventswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsWebappApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventsWebappApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/boiechko/eventswebapp/EventsWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boiechko.eventswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsWebappApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

468fa5c708a233d47d662423c6a723edc6af89f1
Volodymyr-Boiechko <boiechko17@gmail.com>
2022-08-28 22:09:53 +0300

    bug fixes

--- src/main/resources/db/migration/V6__create_auth_token_table.sql
+++ src/main/resources/db/migration/V6__create_auth_token_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS auth_token
 (
     id                       BIGINT AUTO_INCREMENT,
     user_id                  BIGINT,
-    access_token             VARCHAR(200),
+    access_token             VARCHAR(250),
     refresh_token            VARCHAR(200),
     token_type               VARCHAR(20),
     refresh_token_expires_in DATETIME,
